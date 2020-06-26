642d55906d6a2718d5fa595700195f1d445c299d
willy <willy126734@gmail.com>
2020-06-27 05:43:43 +0800

    Second commit

--- Club date Sheet/main.js
+++ Club date Sheet/main.js
@@ -13,9 +13,75 @@ $(document).ready(function(){
  
 });
 
-function setCookie(cname,cvalue,exdays){
+
+ function setTable()
+ {
+     $("#courseTable").empty();
+    //固定標題列
+    $("#courseTable").append(
+        "<tr><th>禮拜</th><th>時間</th><th>複習科目</th><th>修改</th></tr>"
+    );
+
+    //反覆產生資料列
+    let topicCount=topicsArray.length;
+
+    //計算一天有多少毫秒
+    let oneDayMilliseconds=24*60*60*1000;
+    //3個月
+    let monthDays=[31,29,31,30,31,30,31,31,30,31,30,31];
+    let day=['日','一','二','三','四','五','六']
+    for(let x=0; x<7; x++)
+    {
+        //要加天數只能用getTime()加 不然用Date型態加會錯
+        //getTime()單位為毫秒
+        let thisDate= new Date();
+        thisDate.setDate(thisDate.getDate()+x);
+        let trSpecial="<tr>"
+        if(topicsArray[x]=="不上課")
+        {
+            trSpecial="<tr style='background-color:lightyellow'>";
+        }
+        if(getCookie(x.toString())!="")//有cookie
+            $("#courseTable").append(
+                trSpecial+ //用+才能換行
+                "<td>"+ (day[thisDate.getDay()]) +"</td>"+
+                "<td>"+ (thisDate.getMonth()+1)+"/"+thisDate.getDate() + "</td>"+
+                "<td id=td"+x+">"+ getCookie(x.toString())+"</td>"+
+                "<td><input id="+x+" type=\"button\" value=\"編輯\" onclick=\"myMsg("+x+")\"></td>"+
+                "</tr>"
+            ); //每一列有場次、預計日期、主題
+        else
+            $("#courseTable").append(
+                trSpecial+ //用+才能換行
+                "<td>"+ (day[thisDate.getDay()]) +"</td>"+
+                "<td>"+ (thisDate.getMonth()+1)+"/"+thisDate.getDate() + "</td>"+
+                "<td id=td"+x+">"+ topicsArray[x]+"</td>"+
+                "<td><input id="+x+" type=\"button\" value=\"編輯\" onclick=\"myMsg("+x+")\"></td>"+
+                "</tr>"
+            ); //每一列有場次、預計日期、主題
+    }
+ }
+
+ function myMsg(id)
+ {
+    Username=(id).toString();
+    message="請輸入您的行程："
+    let result = prompt(message,"EX:補習/離散數學");
+    if(result!=null)
+    {
+        setCookie(Username,result,30);
+        let tdobj = document.getElementById('td'+id);
+        let a=getCookie(Username);
+        tdobj.innerText = a;
+    }
+    else
+    {}
+ }
+ 
+ //設定cookie格式(document.cookie="username=John Doe; expires=Thu")
+ function setCookie(cname,cvalue,exdays){
     var d = new Date();
-    d.setTime(d.getTime()+(exdays*24*60*60*1000)); //Cookie的時效性為1個月
+    d.setTime(d.getTime()+(exdays*24*60*60*1000));
     var expires = "expires="+d.toGMTString();
     document.cookie = cname+"="+cvalue+"; "+expires;
 }
@@ -29,51 +95,3 @@ function getCookie(cname){
     }
     return "";
 }
-
-function checkCookie(){
-    var user=getCookie("username");
-    if (user!=""){
-        alert("欢迎 " + user + " 再次访问");
-    }
-    else {
-        user = prompt("请输入你的名字:","");
-          if (user!="" && user!=null){
-            setCookie("username",user,30);
-        }
-    }
-}
-
- function setTable()
- {
-     $("#courseTable").empty();
-    //固定標題列
-    $("#courseTable").append(
-        "<tr><th>備註</th><th>時間</th><th>主題</th></tr>"
-    );
-
-    //反覆產生資料列
-    let topicCount=topicsArray.length;
-
-    //計算一天有多少毫秒
-    let oneDayMilliseconds=24*60*60*1000;
-    //3個月
-    let monthDays=[31,29,31,30,31,30,31,31,30,31,30,31];
-    for(let x=0; x<topicCount; x++)
-    {
-        //要加天數只能用getTime()加 不然用Date型態加會錯
-        //getTime()單位為毫秒
-        let thisDate= new Date(startDate.getTime()+1*x*oneDayMilliseconds);
-        let trSpecial="<tr>"
-        if(topicsArray[x]=="不上課")
-        {
-            trSpecial="<tr style='background-color:lightyellow'>";
-        }
-        $("#courseTable").append(
-            trSpecial+ //用+才能換行
-            "<td>"+ (x+1) +"</td>"+
-            "<td>"+ thisDate.toLocaleDateString().slice(5) + "</td>"+
-            "<td>"+ topicsArray[x]+"</td>"+
-            "</tr>"
-        ); //每一列有場次、預計日期、主題
-    }
- }
